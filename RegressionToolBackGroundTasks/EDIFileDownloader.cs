4b109cc489fd0f64c1d390448a9d823c2bb44654
Maninder <maninder.bains@gmail.com>
2016-11-03 18:15:40 +0530

    Intial Commit

---
+++ RegressionToolBackGroundTasks/EDIFileDownloader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegressionToolBackGroundTasks
+{
+    public class EDIFileDownloader
+    {
+
+        public Boolean downloadFile(String url, String FilePath)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(url, FilePath);
+                    return true;
+                }
+            }catch(Exception e)
+            {
+                return false;
+            }
+       
+        }
+
+    }
+}
